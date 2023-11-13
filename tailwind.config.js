9864ded4006b460238aa65109e68a602ca870af4
Philip Marnef <philip@bureaumarnef.be>
2023-11-13 11:11:15 +0100

    Use modern viewports

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,15 @@ module.exports = {
     },
     fontFamily: {
     },
+    screens: {
+      'xs':   '375px',  /* smartphone */
+      'sm':   '640px',  /* phablet | + 335 */
+      'md':   '810px',  /* tablet, portrait | + 170 */
+      'lg':  '1024px',  /* tablet, landscape | + 214 */
+      'xl':  '1366px',  /* laptop | + 342 */
+      '2xl': '1680px',  /* desktop | + 314 */
+      '3xl': '1920px',  /* large screens | + 240 */
+    },
     extend: {
     },
   },
