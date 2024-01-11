81a2bf3c6a67973170f25333c884f26c2fbc9572
Philip Marnef <philip@bureaumarnef.be>
2024-01-11 14:57:14 +0100

    New media queries for hover, no-hover and mobile-only

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,11 +12,14 @@ module.exports = {
     screens: {
       'xs':   '375px',  /* smartphone */
       'sm':   '640px',  /* phablet | + 335 */
+      'max-sm':  { 'max': '809px' },
       'md':   '810px',  /* tablet, portrait | + 170 */
       'lg':  '1024px',  /* tablet, landscape | + 214 */
       'xl':  '1366px',  /* laptop | + 342 */
       '2xl': '1680px',  /* desktop | + 314 */
       '3xl': '1920px',  /* large screens | + 240 */
+      'touch': { 'raw': '(hover: none)' },
+      'notouch':   { 'raw': '(hover: hover)' },
     },
     extend: {
     },
